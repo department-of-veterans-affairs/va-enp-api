0dce8b5983dd2bc34b1f530da3809841723e7272
Kyle MacMillan <kyle.w.macmillan@gmail.com>
2024-09-05 14:47:44 -0400

    Added new v3 notification capability

---
+++ app/v3/notifications/route_schema.py
@@ -0,0 +1,35 @@
+"""Request and Response bodies for v3/notifications."""
+
+from typing import Optional, Union
+
+from pydantic import UUID4, AwareDatetime, BaseModel
+
+
+class NotificationSingleRequest(BaseModel):
+    """Request model for notification endpoint."""
+
+    to: str
+    personalization: Optional[dict[str, str]] = None
+    template: UUID4
+
+    def serialize(self) -> dict[str, Optional[Union[str, dict[str, str]]]]:
+        """Serialize the pydantic model.
+
+        Returns
+        -------
+            dict[str, Optional[Union[str, dict[str, str]]]]: Serialized version of the model
+
+        """
+        serialized = self.model_dump()
+        serialized['template'] = str(serialized['template'])
+        return serialized
+
+
+class NotificationSingleResponse(BaseModel):
+    """Response for notification endpoint."""
+
+    id: UUID4
+    created_at: AwareDatetime
+    updated_at: AwareDatetime
+    sent_at: Optional[AwareDatetime] = None
+    to: str
