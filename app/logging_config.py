f4a02d117f7e7d5d76a04269881656b611f98759
Mackenzie Halliday <mackenziehalliday@Mackenzies-MacBook-Air.local>
2024-09-11 08:35:59 -0400

    Squash #23 - Create initial log config file

---
+++ app/logging_config.py
@@ -0,0 +1,26 @@
+"""Setup global logging configurations using Loguru."""
+
+import sys
+
+from loguru import logger as loguru_logger
+from loguru._logger import Logger
+
+
+def configure_loguru() -> Logger:
+    """Configure Loguru logging.
+
+    Returns
+    -------
+    Logger: Configured Loguru logger instance.
+
+    """
+    # Remove any pre-existing loggers
+    loguru_logger.remove()
+
+    # Add a new logger that logs to stdout
+    loguru_logger.add(sys.stdout, level='DEBUG', format='{time} {level} {message}')
+
+    return loguru_logger
+
+
+logger = configure_loguru()
