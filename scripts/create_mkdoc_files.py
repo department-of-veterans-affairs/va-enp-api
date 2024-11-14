c94fbd7b30c3dba491afb68178bafd7274a56e63
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-11-14 14:33:27 -0500

    Squash #54 - Swap SUMMARY for index

--- scripts/create_mkdoc_files.py
+++ scripts/create_mkdoc_files.py
@@ -33,5 +33,5 @@ for path in sorted(src.rglob('*.py')):
 
     mkdocs_gen_files.set_edit_path(full_doc_path, Path('../') / path)
 
-with mkdocs_gen_files.open('SUMMARY.md', 'w') as nav_file:
+with mkdocs_gen_files.open('index.md', 'w') as nav_file:
     nav_file.writelines(nav.build_literate_nav())
