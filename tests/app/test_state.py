2363d9c73516ef80fae32e9a88c6646a772c9e19
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-12-23 13:41:53 -0600

    Squash 78 - Correct testing

--- tests/app/test_state.py
+++ tests/app/test_state.py
@@ -7,7 +7,7 @@ from app.state import ENPState
 def test_enp_state_initialization() -> None:
     """Test to make sure ENPState can have provider attribute."""
     state = ENPState()
-    state.providers = {'aws': ProviderAWS()}
+    assert isinstance(state.providers, dict)
     assert 'aws' in state.providers
     assert isinstance(state.providers['aws'], ProviderAWS)
 
