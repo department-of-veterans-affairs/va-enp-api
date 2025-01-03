f04fd0a097ae862b9aced6a5818c98d38c35fbdb
Kyle MacMillan <16893311+k-macmillan@users.noreply.github.com>
2025-01-03 16:15:00 -0500

    Added poetry version check

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
 
+POETRY_VERSION=$(grep "poetry_version" pyproject.toml | grep -m 1 -oE "[0-9]{1}.[0-9]{1,3}.[0-9]{1,3}")
+
+if [[ -z $(head -1 poetry.lock | grep -E "${POETRY_VERSION}") ]]; then
+  echo -E "Expected Poetry version: ${POETRY_VERSION}, found: $(head -1 poetry.lock | grep -oE "[0-9]{1}.[0-9]{1,3}.[0-9]{1,3}")"
+  exit 1
+fi
+
 # Values set in pyroject.toml
 pytest
