94f68106d5b62baa03ec99fefc9379aa1efc9503
Kyle MacMillan <16893311+k-macmillan@users.noreply.github.com>
2025-01-30 16:24:41 -0500

    Renamed due to formatter thinking it was a test

--- app/main.py
+++ app/main.py
@@ -111,9 +111,9 @@ def simple_route() -> dict[str, str]:
 
 
 @app.post('/db/test', status_code=status.HTTP_201_CREATED, dependencies=[Depends(JWTBearer())])
-async def test_db_create(
+async def db_create_test(
     *,
-    data: str = 'hello',
+    data: str,
     db_session: Annotated[async_scoped_session[AsyncSession], Depends(get_write_session_with_depends)],
 ) -> dict[str, list[dict[str, str]]]:
     """Test inserting Templates and Notifications into the database. This is a temporary test endpoint.
@@ -220,7 +220,7 @@ async def fetch_notifications(session: AsyncSession, year_list: List[int]) -> Li
 
 
 @app.get('/db/test', status_code=status.HTTP_200_OK, dependencies=[Depends(JWTBearer())])
-async def test_db_read(
+async def db_read_test(
     db_session: Annotated[async_scoped_session[AsyncSession], Depends(get_read_session_with_depends)],
     years: str | None = Query(
         default=None, description='Comma-separated years to filter notifications (e.g., 2024,2025)'
