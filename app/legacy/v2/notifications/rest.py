9cc308101b8763fd006df19e499b2841b4e6646b
Michael Wellman <mchlwellman@gmail.com>
2025-03-27 09:54:43 -0400

    Added template id to context as an example

--- app/legacy/v2/notifications/rest.py
+++ app/legacy/v2/notifications/rest.py
@@ -5,6 +5,7 @@ from uuid import uuid4
 
 from fastapi import APIRouter, BackgroundTasks, Body, Depends, Request, status
 from pydantic import UUID4
+from starlette_context import context
 
 from app.auth import JWTBearer
 from app.constants import NotificationType
@@ -97,6 +98,7 @@ async def create_sms_notification(
     Returns:
         V2PostSmsResponseModel: The notification response data if notification is created successfully.
     """
+    context['template_id'] = request.template_id
     logger.debug('Received SMS request with data: {}', request)
 
     try:
