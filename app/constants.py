d11d223293d6ae99d7b47ef5309917429e5a5db4
Michael Wellman <mchlwellman@gmail.com>
2025-04-28 12:49:43 -0400

    Removed unused queues

--- app/constants.py
+++ app/constants.py
@@ -54,13 +54,6 @@ class OSPlatformType(StrEnum):
 class QueueNames(StrEnum):
     """Celery queue names."""
 
-    CALLBACKS = 'service-callbacks'
-    COMMUNICATION_ITEM_PERMISSIONS = 'communication-item-permissions'
-    DELIVERY_STATUS_RESULT_TASKS = 'delivery-status-result-tasks'
     LOOKUP_CONTACT_INFO = 'lookup-contact-info-tasks'
     LOOKUP_VA_PROFILE_ID = 'lookup-va-profile-id-tasks'
-    NOTIFY = 'notify-internal-tasks'
-    PERIODIC = 'periodic-tasks'
-    RETRY = 'retry-tasks'
-    SEND_EMAIL = 'send-email-tasks'
     SEND_SMS = 'send-sms-tasks'
